79c7c615fb610801770204a423bb856ff72d4ad9
Ray Valdez <ray_valdez@hotmail.co.uk>
2019-01-04 23:48:51 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,13 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    raise params.inspect
+    @user = User.find_by(username: params["username"], password: params["password"])
+    if @user
+      session[:user_id] = @user.id
+      redirect to '/account'
+    else
+      erb :error
+    end
   end
 
   get '/account' do
